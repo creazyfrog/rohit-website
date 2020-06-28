042eb2b272585f390ffff28d3ce1d46bcbcab11e
rosharma <rohitcse.gec@gmail.com>
2020-06-28 23:56:01 +0530

    Updates

---
+++ precache-manifest.0c2384a76f648ead80b9aea61d530f04.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c90245283da01a1d29f0efdc35322b18",
+    "url": "/rohit-website/index.html"
+  },
+  {
+    "revision": "d65cd05c68448e22b554",
+    "url": "/rohit-website/static/css/main.88be113e.chunk.css"
+  },
+  {
+    "revision": "9756e35f60564e6d8570",
+    "url": "/rohit-website/static/js/2.5199eb70.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rohit-website/static/js/2.5199eb70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d65cd05c68448e22b554",
+    "url": "/rohit-website/static/js/main.9b2df7ae.chunk.js"
+  },
+  {
+    "revision": "375e579d88eef54713d0",
+    "url": "/rohit-website/static/js/runtime-main.9ef98838.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rohit-website/static/media/logo.5d5d9eef.svg"
+  }
+]);
